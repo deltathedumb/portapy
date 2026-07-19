7b120675992225867ef5348bb55988e95b2d88f3
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-19 16:09:21 -0500

    Define provisional numeric opaque-handle ABI

--- include/portapy.h
+++ include/portapy.h
@@ -24,8 +24,11 @@ extern "C" {
 
 #define PORTAPY_ABI_VERSION 1u
 
-typedef struct portapy_runtime portapy_runtime;
-typedef struct portapy_value portapy_value;
+typedef uint64_t portapy_runtime;
+typedef uint64_t portapy_value;
+
+#define PORTAPY_NULL_RUNTIME ((portapy_runtime)0)
+#define PORTAPY_NULL_VALUE ((portapy_value)0)
 
 typedef enum portapy_status {
     PORTAPY_OK = 0,
@@ -63,38 +66,40 @@ typedef struct portapy_config {
     void *host_context;
 } portapy_config;
 
-typedef struct portapy_error_info {
-    portapy_status status;
-    portapy_bytes_view type_name_utf8;
-    portapy_bytes_view message_utf8;
-    portapy_bytes_view traceback_utf8;
-} portapy_error_info;
-
+PORTAPY_API portapy_status PORTAPY_CALL portapy_library_initialize(void);
 PORTAPY_API uint32_t PORTAPY_CALL portapy_abi_version(void);
-PORTAPY_API const char *PORTAPY_CALL portapy_version_string(void);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_runtime_create(const portapy_config *, portapy_runtime **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_runtime_destroy(portapy_runtime *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_runtime_interrupt(portapy_runtime *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_exec_utf8(portapy_runtime *, const uint8_t *, size_t, const uint8_t *, size_t);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_eval_utf8(portapy_runtime *, const uint8_t *, size_t, const uint8_t *, size_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_get_global_utf8(portapy_runtime *, const uint8_t *, size_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_call(portapy_runtime *, portapy_value *, portapy_value *const *, size_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_retain(portapy_runtime *, portapy_value *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_release(portapy_runtime *, portapy_value *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_get_kind(portapy_runtime *, portapy_value *, portapy_value_kind *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_none(portapy_runtime *, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_bool(portapy_runtime *, int, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_i64(portapy_runtime *, int64_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_f64(portapy_runtime *, double, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_utf8(portapy_runtime *, const uint8_t *, size_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_bytes(portapy_runtime *, const uint8_t *, size_t, portapy_value **);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_bool(portapy_runtime *, portapy_value *, int *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_i64(portapy_runtime *, portapy_value *, int64_t *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_f64(portapy_runtime *, portapy_value *, double *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_utf8(portapy_runtime *, portapy_value *, portapy_bytes_view *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_bytes(portapy_runtime *, portapy_value *, portapy_bytes_view *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_last_error(portapy_runtime *, portapy_error_info *);
-PORTAPY_API portapy_status PORTAPY_CALL portapy_clear_error(portapy_runtime *);
+
+PORTAPY_API portapy_status PORTAPY_CALL portapy_runtime_create(
+    const portapy_config *config,
+    portapy_runtime *out_runtime
+);
+PORTAPY_API portapy_status PORTAPY_CALL portapy_runtime_destroy(
+    portapy_runtime runtime
+);
+
+PORTAPY_API portapy_status PORTAPY_CALL portapy_value_from_i64(
+    portapy_runtime runtime,
+    int64_t value,
+    portapy_value *out_value
+);
+PORTAPY_API portapy_status PORTAPY_CALL portapy_value_get_kind(
+    portapy_runtime runtime,
+    portapy_value value,
+    portapy_value_kind *out_kind
+);
+PORTAPY_API portapy_status PORTAPY_CALL portapy_value_as_i64(
+    portapy_runtime runtime,
+    portapy_value value,
+    int64_t *out_value
+);
+PORTAPY_API portapy_status PORTAPY_CALL portapy_value_retain(
+    portapy_runtime runtime,
+    portapy_value value
+);
+PORTAPY_API portapy_status PORTAPY_CALL portapy_value_release(
+    portapy_runtime runtime,
+    portapy_value value
+);
 
 #ifdef __cplusplus
 }

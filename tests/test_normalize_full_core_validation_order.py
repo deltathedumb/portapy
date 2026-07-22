084d918361127cbc56ee266e89c1895161f785ee
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-22 18:39:47 -0500

    Test pattern collision pipeline wiring

--- tests/test_normalize_full_core_validation_order.py
+++ tests/test_normalize_full_core_validation_order.py
@@ -8,9 +8,9 @@ from tools import normalize_full_core_validation as validation
 def test_match_defaults_precede_constructor_collision_renames() -> None:
     source = inspect.getsource(validation.main)
 
-    assert source.index('(\"extended_semantics\"') < source.index(
-        '(\"expr_stmt_initializer\"'
-    )
-    assert source.index('(\"expr_stmt_initializer\"') < source.index(
-        '(\"native_node_fields\"'
-    )
+    extended = source.index('(\"extended_semantics\"')
+    expr_stmt = source.index('(\"expr_stmt_initializer\"')
+    pattern_collision = source.index('(\"pattern_constructor_collisions\"')
+    node_fields = source.index('(\"native_node_fields\"')
+
+    assert extended < expr_stmt < pattern_collision < node_fields
